d6728c10169782aa6d7cc43cd10e9cecb5d40bb7
divineamunega <divineamunega@gmail.com>
2024-10-05 15:05:05 +0100

    taking another step in C

--- C Primer/Chapter_2/main.c
+++ C Primer/Chapter_2/main.c
@@ -10,3 +10,14 @@
 
 //     return 0;
 // }
+
+// Taking another step in C
+int main(void)
+{
+    int feet, fathoms;
+    fathoms = 2;
+    feet = 6 * fathoms;
+    printf("There are  %d feet in %d fathoms! \n", feet, fathoms);
+    printf("Yes, I said %d feet! \n", feet);
+    return 0;
+}
