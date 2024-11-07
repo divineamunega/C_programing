d2e00df2d2613fdd052f45de9620d2f8349ceb86
divineamunega <divineamunega@gmail.com>
2024-11-07 12:48:07 +0100

    mod: 8showf_pt.c

--- C Primer/Chapter_3/8showf_pt.c
+++ C Primer/Chapter_3/8showf_pt.c
@@ -8,12 +8,12 @@ int main(void)
     double abet = 2.12e19;
     long double dip = 5.32e-5;
 
-    printf("%f can be written %e \n", aboat, aboat);
+    printf("%f can be written %e \n\n", aboat, aboat);
     // Nexct line requires C99 or latercompliance
 
-    printf("And it's %a in hexadecimal, powers of 2 notation \n", aboat);
-    printf("%f can be written %e \n", abet, abet);
-    printf("%Lf can be written %Le \n", dip, dip);
+    printf("And it's %a in hexadecimal, powers of 2 notation \n\n", aboat);
+    printf("%f can be written %e \n\n", abet, abet);
+    printf("%Lf can be written %Le and %La \n\n", dip, dip, dip);
 
-        return 0;
+    return 0;
 }
