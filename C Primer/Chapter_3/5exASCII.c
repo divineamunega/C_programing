1da730d3b9f0e08875b821045f8607e6d9b9543b
divineamunega <divineamunega@gmail.com>
2025-01-03 00:51:44 +0100

    3: 5exASCII

---
+++ C Primer/Chapter_3/5exASCII.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int code;
+
+    printf("Please Enter an ASCII code \n");
+    scanf("%d", &code);
+
+    printf("The character for that is %c \n", code);
+}
